c365b75446f888f5f022b7a776cfadd53f771c8c
alikiskandaryan <alikiskandaryan9408@gmail.com>
2023-10-31 16:02:20 +0400

    Adding Simple API

--- index.js
+++ index.js
@@ -38,15 +38,19 @@ const url = require('url');
 // /////////////////////////////////////////////
 // SERVER & ROUTING
 
-const server = http.createServer((req,res) => {
-    console.log(req.url);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8', );
+const dataObject = JSON.parse(data);
 
+const server = http.createServer((req,res) => {
     const pathName = req.url;
 
     if(pathName === '/' || pathName === '/overview'){
         res.end('This is the overview');
     }else if( pathName === '/product'){
         res.end('This is the product');
+    }else if (pathName ==='/api'){
+        res.writeHead(200, {"Content-type" : "application/json"});
+        res.end(data);
     }else{
         res.writeHead(404, {
             "Content-type" : "text/html",
