a93b32ed876d9c26cf73d46903bb2e3ca81e55b3
alikiskandaryan <alikiskandaryan9408@gmail.com>
2023-10-31 15:39:21 +0400

    server & Routing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 // /////////////////////////////////////////////
 // FILES
@@ -35,13 +36,28 @@ const http = require('http');
 
 
 // /////////////////////////////////////////////
-// SERVER
+// SERVER & ROUTING
 
 const server = http.createServer((req,res) => {
-    console.log(req);
-    res.end('Hello from the server!');
+    console.log(req.url);
+
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is the overview');
+    }else if( pathName === '/product'){
+        res.end('This is the product');
+    }else{
+        res.writeHead(404, {
+            "Content-type" : "text/html",
+            "my-own-header" : "hello-world"
+        });
+        res.end('Page not found');
+    }
+
 });
 
 server.listen(8000,'127.0.0.1' , () =>{
     console.log('Listening to request on port 8000');
 });
+
